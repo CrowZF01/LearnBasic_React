1ce5d7d1078b500c45dbe0412e5f37d4350490a1
71241153_Felix_Benedictus_Setiawan <123315666+CrowZF01@users.noreply.github.com>
2026-07-27 20:52:20 +0700

    styling learn

---
+++ src/Button.jsx
@@ -0,0 +1,18 @@
+
+function Button() {
+
+    const styles = {
+        backgroundColor: "black",
+        color: "white",
+        padding: "10px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+    }
+
+    return (
+        <button style={styles}>Click Me</button>
+    )
+}
+
+export default Button
