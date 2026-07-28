a2794a8f8509e0ce12f27fd6193184e3d28330cb
71241153_Felix_Benedictus_Setiawan <123315666+CrowZF01@users.noreply.github.com>
2026-07-28 17:32:48 +0700

    func

--- src/List.jsx
+++ src/List.jsx
@@ -6,9 +6,17 @@ function List() {
     { id: 3, name: "Orange", calories: 45 },
     { id: 4, name: "Mango", calories: 60 }];
 
+    //Alphabet
+    // fruits.sort((a, b) => a.name.localeCompare(b.name));
+    // fruits.sort((a, b) => b.name.localeCompare(a.name));
 
-    const listItems = fruits.map(fruit => <li key={fruit.id}>
-        {fruit.name}: &nbsp; {fruit.calories}</li>);
+    //Numeric
+    fruits.sort((a, b) => a.calories - b.calories)
+
+    const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+
+    const listItems = lowCalFruits.map(lowCalFruits => <li key={lowCalFruits.id}>
+        {lowCalFruits.name}: &nbsp; {lowCalFruits.calories}</li>);
 
     return (<ol>{listItems}</ol>);
 }
