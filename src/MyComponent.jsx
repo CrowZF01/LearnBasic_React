ea2a15cdf423ac0ae801b1bb699fa266b6cce298
71241153_Felix_Benedictus_Setiawan <123315666+CrowZF01@users.noreply.github.com>
2026-07-31 09:47:47 +0700

    Basic onchange vent handler

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -5,6 +5,9 @@ function MyComponent() {
 
     const [name, setName] = useState("Guest");
     const [quantity, setQuantity] = useState(1);
+    const [comment, setComment] = useState("");
+    const [payment, setPayment] = useState("");
+    const [shipping, setShipping] = useState("Delivery");
 
     function handleNameChange(event) {
         setName(event.target.value)
@@ -12,6 +15,18 @@ function MyComponent() {
 
     function handleQuantityChange(event) {
         setQuantity(event.target.value)
+    }
+
+    function handleCommentChange(event) {
+        setComment(event.target.value)
+    }
+
+    function handlePaymentChange(event) {
+        setPayment(event.target.value)
+    }
+
+    function handleShippingChange(event) {
+        setShipping(event.target.value)
     }
 
     return (
@@ -22,6 +37,33 @@ function MyComponent() {
 
             <input value={quantity} onChange={handleQuantityChange} type="number" />
             <p>Quantity: {quantity}</p>
+
+            <textarea value={comment} onChange={handleCommentChange}
+                placeholder="Enter delivery instructions"></textarea>
+            <p>Comment: {comment}</p>
+
+            <select value={payment} onChange={handlePaymentChange}>
+                <option value="">Select a payment method</option>
+                <option value="Visa">Visa</option>
+                <option value="Mastercard">Mastercard</option>
+                <option value="Giftcard">Giftcard</option>
+            </select>
+            <p>Payment Method: {payment}</p>
+
+            <label>
+                <input type="radio" value="Pick Up"
+                    checked={shipping === "Pick Up"}
+                    onChange={handleShippingChange} />
+                Pick Up
+            </label>
+            <br />
+            <label>
+                <input type="radio" value="Delivery"
+                    checked={shipping === "Delivery"}
+                    onChange={handleShippingChange} />
+                Delivery
+            </label>
+            <p>Shipping: {shipping}</p>
 
         </div>
     )
