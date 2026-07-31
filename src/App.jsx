b2098e2b426e6b4ebd7dc49f1d3e3af8e15cbc2e
71241153_Felix_Benedictus_Setiawan <123315666+CrowZF01@users.noreply.github.com>
2026-07-31 09:59:46 +0700

    colorPICKER

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import MyComponent from "./MyComponent";
 import Counter from "./Counter"
+import ColorPicker from "./ColorPicker";
 
 function App() {
   return (
     <>
-      <MyComponent />
-      <Counter />
+      <ColorPicker />
     </>
 
   )
